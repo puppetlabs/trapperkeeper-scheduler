91930b021fcf25d8c3e78e90bf1c8023e0114e15
Chris Price <chris@puppetlabs.com>
2015-06-19 12:45:51 +0100

    (MAINT) 0.0.1 release, bumped to 0.1.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def tk-version "1.1.1")
 (def ks-version "1.0.0")
 
-(defproject puppetlabs/trapperkeeper-scheduler "0.0.1"
+(defproject puppetlabs/trapperkeeper-scheduler "0.1.0-SNAPSHOT"
   :description "Trapperkeeper Scheduler Service"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
@@ -14,14 +14,19 @@
 
   :test-paths ["test/unit" "test/integration"]
 
+  :test-selectors {:integration :integration
+                   :unit (complement :integration)}
+
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :username :env/clojars_jenkins_username
+                                     :password :env/clojars_jenkins_password
+                                     :sign-releases false}]]
+
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]
                                   [puppetlabs/trapperkeeper ~tk-version :classifier "test" :scope "test"]
                                   [puppetlabs/kitchensink ~ks-version :classifier "test" :scope "test"]
                                   [spyscope "0.1.4" :exclusions [clj-time]]]
                    :injections [(require 'spyscope.core)]}}
-
-  :test-selectors {:integration :integration
-                   :unit (complement :integration)}
 
   :repl-options {:init-ns user})
