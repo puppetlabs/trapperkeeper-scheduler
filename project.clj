a450ec8b321d167413639fdf353e71356aec36b6
Jenkins CI <ci@puppet.com>
2018-10-22 16:57:08 +0000

    Version 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/trapperkeeper-scheduler "1.0.1-SNAPSHOT"
+(defproject puppetlabs/trapperkeeper-scheduler "1.0.1"
   :description "Trapperkeeper Scheduler Service"
 
   :dependencies [[org.clojure/clojure]
