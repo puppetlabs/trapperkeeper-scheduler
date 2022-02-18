b8d6dcec960efe37675814e3b62151487bdb0846
Maggie Dreyer <maggie@puppet.com>
2022-02-18 09:01:40 -0800

    (maint) Update clj-parent to latest

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
 
   :min-lein-version "2.9.1"
 
-  :parent-project {:coords [puppetlabs/clj-parent "4.6.17"]
+  :parent-project {:coords [puppetlabs/clj-parent "4.9.4"]
                    :inherit [:managed-dependencies]}
 
   :pedantic? :abort
