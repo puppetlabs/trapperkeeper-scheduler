069a2245a84c820a0a5ab76c425bf3a19139a383
Arthur Lawson <artlawson318@gmail.com>
2024-06-05 15:44:25 -0700

    Adds mend scanning and gh workflow
    Removes `.travis.yml` and `snyk_merge.yaml` as those are no longer in use. Replaces them with gh action pr testing and `mend.yaml`

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/trapperkeeper-scheduler "1.1.4-SNAPSHOT"
+(defproject puppetlabs/trapperkeeper-scheduler "1.2.0-SNAPSHOT"
   :description "Trapperkeeper Scheduler Service"
 
   :dependencies [[org.clojure/clojure]
